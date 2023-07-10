080bf4007a6cbd26d5d8d9f88f217c2db506873d
BHUPESH BAJPAI <bhupeshbajpai@BHUPESHs-MacBook-Air.local>
2023-07-10 21:33:27 +0530

    update

--- week-3/03-course-app-medium/index.js
+++ week-3/03-course-app-medium/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs');
+const path = require('path');
+
+const fs = require ('fs')
 
 app.use(express.json());
 
@@ -7,9 +12,65 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+const secretKey = 'your-secret-key';
+
+
+const adminFilePath = path.join(__dirname, 'admin.json');
+
+// Middleware to read JSON files
+function readJsonFile(filePath) {
+  return function(req, res, next) {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+        return;
+      }
+
+      req.jsonData = JSON.parse(data);
+      next();
+    });
+  };
+}
+
 // Admin routes
-app.post('/admin/signup', (req, res) => {
+app.post('/admin/signup',readJsonFile(adminFilePath), (req, res) => {
   // logic to sign up admin
+
+  const adminsJson = req.jsonData;
+
+  const{username,password} = req.body;
+  if(adminsJson.some(admins => admins.username === username)){
+    return res.status(400).send("admin already exist");
+  }
+
+  bcrypt.genSalt(10,(err,salt)=>{
+    if(err){
+      return res.status(500).json({
+        message : "error in genrating salt"
+      })
+     
+    }
+    bcrypt.hash(password,salt,(err,hash)=>{
+      if(err){
+        return res.status(500).json({
+          message : "error in hashing password"
+        })
+      }
+      const admin={
+        username,
+        password : hash
+      };
+      adminsJson.push(admin);
+  
+      const token = jwt.sign({ username, isAdmin: true }, secretKey, { expiresIn: '1h' });
+  
+        // Return the token to the client
+        res.json({ message: 'Admin created successfully', token });
+    })
+  
+  })
+
 });
 
 app.post('/admin/login', (req, res) => {
